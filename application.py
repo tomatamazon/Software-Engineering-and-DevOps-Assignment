18ae9316bd7ddcda97ad40f84bdb8a27978a19b5
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-08-23 15:03:40 +0100

    Modify return message

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory, render_template
+from sshtunnel import SSHTunnelForwarder
+import pymysql
 
 application = app = Flask(__name__)
 
 @application.route('/')
 def home():
-    return 'Hello Em!'
+    return 'Hello Elastic Beanstalk!'
 
 if __name__ == "__main__":
     application.run()
-
