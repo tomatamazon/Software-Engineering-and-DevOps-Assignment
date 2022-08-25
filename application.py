e54ed46738847385a8be6a4785ce907ab91590f7
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-08-25 18:44:36 +0100

    Implement React build folder and hook flask backend to React

--- application.py
+++ application.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, send_from_directory, render_template
-from sshtunnel import SSHTunnelForwarder
-import pymysql
+# from sshtunnel import SSHTunnelForwarder
+# import pymysql
+import open_pass
 
-application = app = Flask(__name__)
+application = app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+ec2_dns = "ec2-54-89-239-77.compute-1.amazonaws.com"
+db_pass = open_pass.get_pass()
 
-@application.route('/')
-def home():
-    return 'Hello Elastic Beanstalk!'
+@application.route("/", defaults={'path': ''})
+def home(path):
+    return send_from_directory(application.static_folder, "index.html")
 
-if __name__ == "__main__":
-    application.run()
+# if __name__ == "__main__":
+#     application.run()
