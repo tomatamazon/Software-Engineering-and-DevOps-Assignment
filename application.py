88a040a63d6569d895bdbed7a280b2a57130399c
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-08-31 10:13:58 +0100

    Add get_keys and get_pass functions for security reasons

--- application.py
+++ application.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, send_from_directory, render_template
 from sshtunnel import SSHTunnelForwarder
 import pymysql
-import open_pass
+import boto3
+import base64
+from botocore.exceptions import ClientError
+import open_pass, get_pass, get_keys
 
 application = app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 ec2_dns = "ec2-54-89-239-77.compute-1.amazonaws.com"
-db_pass = open_pass.get_pass()
+# db_pass = open_pass.get_pass()
+db_pass = get_pass.get_pass(boto3, base64, ClientError)
+db_keys = get_keys.get_keys(boto3, base64, ClientError)
+
 
 @application.route("/", defaults={'path': ''})
 def home(path):
---
+++ get_keys.py
@@ -0,0 +1,42 @@
+# This function gets the database keys (.pem file)
+def get_keys(boto3, base64, ClientError):
+    secret_name = "db-keys.pem"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client (
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager cannot decrypt the protected secret using the provided KMS key.
+            raise "Decryption Error. Secrets Manager cannot decrypt the protected secret using the provided KMS key."
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            raise "Internal Service Error. There is an error on the server side. Please try again later."
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            raise "Invalid Parameter. You provided an invalid value for a parameter."
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            raise "Invalid Request. You provided a parameter value that is not valid for the current state of the resource."
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # The resource requested cannot be found.
+            raise "Resource Not Found. The resource that you requested could not be found."
+    else:
+        # Decrypts secret using the associated KMS key.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+        if secret is not None:
+            return secret
+        if decoded_binary_secret is not None:
+            return decoded_binary_secret
