3419e26eef47f6b52e7938353becb310faf4b320
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-08-31 15:44:55 +0100

    Revert to previous

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,15 @@ import HomePage from "./HomePage";
 
 
 function App() {
-    return (
-        <div>
-            Testing React
-        </div>
-    );
-    // const [accessLevel, setAccessLevel] = useState("");
-    //
-    // // Maybe add the below access logic onto the backend to prevent against broken access control attacks.
-    // if (accessLevel === "" || accessLevel === "invalid") {
-    //     return (<LoginPage setAccessLevel={setAccessLevel}/>);
-    // } else {
-    //     return (<HomePage accessLevel={accessLevel} setAccessLevel={setAccessLevel}/>);
-    // }
+
+    const [accessLevel, setAccessLevel] = useState("");
+
+    // Maybe add the below access logic onto the backend to prevent against broken access control attacks.
+    if (accessLevel === "" || accessLevel === "invalid") {
+        return (<LoginPage setAccessLevel={setAccessLevel}/>);
+    } else {
+        return (<HomePage accessLevel={accessLevel} setAccessLevel={setAccessLevel}/>);
+    }
 }
 
 export default App;
