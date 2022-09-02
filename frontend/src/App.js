d4139e051c3876b9d15f1991f3f47d5754b8cabe
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-09-02 14:34:50 +0100

    Modify App.js to return a different page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,19 +6,14 @@ import HomePage from "./HomePage";
 
 
 function App() {
-    return(
-        <div>
-            Yay you can see this change!
-        </div>
-    )
-    // const [accessLevel, setAccessLevel] = useState("");
-    //
-    // // Maybe add the below access logic onto the backend to prevent against broken access control attacks.
-    // if (accessLevel === "" || accessLevel === "invalid") {
-    //     return (<LoginPage setAccessLevel={setAccessLevel}/>);
-    // } else {
-    //     return (<HomePage accessLevel={accessLevel} setAccessLevel={setAccessLevel}/>);
-    // }
+    const [accessLevel, setAccessLevel] = useState("");
+
+    // Maybe add the below access logic onto the backend to prevent against broken access control attacks.
+    if (accessLevel === "" || accessLevel === "invalid") {
+        return (<LoginPage setAccessLevel={setAccessLevel}/>);
+    } else {
+        return (<HomePage accessLevel={accessLevel} setAccessLevel={setAccessLevel}/>);
+    }
 }
 
 export default App;
