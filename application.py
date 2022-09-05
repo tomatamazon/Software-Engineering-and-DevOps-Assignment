ab36044f8c55fafb33c8efbc5e1ad11b58ab2166
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-09-05 17:56:32 +0100

    Remove logging and change print statement

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, send_from_directory, render_template
-import logging
 from sshtunnel import SSHTunnelForwarder
 import pymysql
 import boto3
@@ -10,8 +9,6 @@ import get_pass, get_keys, login
 application = app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 ec2_dns = "ec2-54-89-239-77.compute-1.amazonaws.com"
 db_pass = get_pass.get_pass(boto3, base64, ClientError)
-db_pass = db_pass[0]
-logging.info(db_pass)
 db_keys = get_keys.get_keys(boto3, base64, ClientError)
 
 @application.route("/", defaults={'path': ''})
