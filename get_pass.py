0a01420829dfd3a0eab0a7a546361df8d85284e3
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-09-05 16:43:19 +0100

    Remove majority of logging

--- get_pass.py
+++ get_pass.py
@@ -10,43 +10,31 @@ def get_pass(boto3, base64, ClientError):
     )
 
     try:
-        print("Trying here (try)")
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
-        print("get_secret_value_response is", get_secret_value_response)
-        print("this should work now")
 
     except ClientError as e:
         if e.response['Error']['Code'] == 'DecryptionFailureException':
             # Secrets Manager cannot decrypt the protected secret using the provided KMS key.
-            print("e1")
             raise "Decryption Error. Secrets Manager cannot decrypt the protected secret using the provided KMS key."
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
             # An error occurred on the server side.
-            print("e2")
             raise "Internal Service Error. There is an error on the server side. Please try again later."
         elif e.response['Error']['Code'] == 'InvalidParameterException':
             # You provided an invalid value for a parameter.
-            print("e3")
             raise "Invalid Parameter. You provided an invalid value for a parameter."
         elif e.response['Error']['Code'] == 'InvalidRequestException':
             # You provided a parameter value that is not valid for the current state of the resource.
-            print("e4")
             raise "Invalid Request. You provided a parameter value that is not valid for the current state of the resource."
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
             # The resource requested cannot be found.
-            print("e5")
             raise "Resource Not Found. The resource that you requested could not be found."
         else:
-            print("We're still here")
             raise e
     else:
-        print("Hola 4")
         # Decrypts secret using the associated KMS key.
         if 'SecretString' in get_secret_value_response:
-            print(get_secret_value_response)
             return get_secret_value_response['SecretString']
         else:
-            print(get_secret_value_response)
             return base64.b64decode(get_secret_value_response['SecretBinary'])
