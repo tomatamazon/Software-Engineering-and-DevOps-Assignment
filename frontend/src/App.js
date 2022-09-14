fa65e11923045c99f63c774c6706caa75364e415
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-09-14 13:02:36 +0100

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,24 @@ import LoginPage from './LoginPage';
 import HomePage from './HomePage';
 
 function App() {
+
+    // const accessLevel = "";
+    //
+    // const setAccessLevel = (userPerms) => {
+    //     console.log("userPerms is", userPerms)
+    // };
     
     const [accessLevel, setAccessLevel] = useState("");
 
     // Maybe add the below access logic onto the backend to prevent against broken access control attacks.
     if (accessLevel === "" || accessLevel === "invalid") {
-        return (<LoginPage setAccessLevel={setAccessLevel}/>);
+        // return (<LoginPage setAccessLevel={setAccessLevel}/>);
+        // return (<LoginPage accessLevel={accessLevel} setAccessLevel={setAccessLevel} />);
+        return (
+            <div>
+                <p>Hi Marcel</p>
+            </div>
+        )
     } else {
         return (<HomePage accessLevel={accessLevel} setAccessLevel={setAccessLevel}/>);
     };
