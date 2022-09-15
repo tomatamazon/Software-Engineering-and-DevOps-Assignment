9e082caa73bfb374dca25411e554d0407b30ecde
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-09-15 11:24:10 +0100

    Fix incorrect import

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import pymysql
 import boto3
 import base64
 from botocore.exceptions import ClientError
-import get_pass, login, post_data, get_entries, update_entry, delete_entry
+import get_pass, login, create_ticket, get_entries, update_entry, delete_entry
 
 application = app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 ec2_dns = "ec2-54-89-239-77.compute-1.amazonaws.com"
