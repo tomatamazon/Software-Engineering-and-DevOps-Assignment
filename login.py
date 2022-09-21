1607a06c098bb2c35d472d6c7d303185f80ec47f
tomatamazon <tomsheehy12@yahoo.co.uk>
2022-09-21 11:09:59 +0100

    Add encrypted user to database

--- login.py
+++ login.py
@@ -1,10 +1,19 @@
+import bcrypt
+
 from flask import jsonify, make_response
 from application import request, SSHTunnelForwarder, pymysql
 
 def login(ec2_dns, db_pass):
 
-    username = request.json["username"]
-    password = request.json["password"]
+    # username = request.json["username"]
+    # password = request.json["password"]
+
+    username = "admin"
+    password = "admin"
+
+    password = password.encode('utf-8')
+
+    username = bcrypt.hashpw(password, bcrypt.gensalt(10))
 
     # get_login_query = "SELECT * FROM db.users WHERE Username='" + username + "' AND Password='" + password + "';"
     # get_login_query = "SELECT * FROM db.users WHERE Username=(%(username)s) AND Password=(%(password)s);"
@@ -28,7 +37,8 @@ def login(ec2_dns, db_pass):
             with conn.cursor() as cur:
                 # If the query was successful, 1 will be returned.
                 # If the query was unsuccessful, 0 will be returned.
-                response = cur.execute("""SELECT * FROM db.users WHERE Username = %(username)s AND Password = %(password)s""", {'username': username, 'password': password})
+                # response = cur.execute("""SELECT * FROM db.users WHERE Username = %(username)s AND Password = %(password)s""", {'username': username, 'password': password})
+                response = cur.execute("""INSERT INTO db.users (Username, Password) VALUES (%(username)s, %(password)s);""", {'username': username, 'password': password})
                 if response == 1:
                     for row in cur:
                         user_type = row[1]
